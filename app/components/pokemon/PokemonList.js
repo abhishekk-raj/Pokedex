67b35838de33bd60e312b25db974fae9baa93321
Abhishek Raj <abhishek@betatestsolutions.com>
2020-07-12 02:56:05 +0530

    Create ListView to show list of Pokemon data

--- app/components/pokemon/PokemonList.js
+++ app/components/pokemon/PokemonList.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 
-const PokemonListComponent = ({ navigation }) => {
-  
-  const pressHandler = () => {
-    navigation.navigate('PokemonDetail')
-  }
-  return (
-    <View>
-      <Text>ListComponent</Text>
-      <Button title='go to detail' onPress={pressHandler}/>
-    </View>
-  );
+import { MOCKUP_DATA } from "../../shared/data";
+import { commonStyles } from "../../styles/common";
+
+const PokemonListComponent = ({navigation}) => {
+	
+	const [pokemonData, setPokemonData] = useState(MOCKUP_DATA);
+	
+	return (
+		<View>
+			<FlatList data={pokemonData}
+								renderItem={({item}) => (
+									<TouchableOpacity
+										style={commonStyles.listItem}
+										onPress={() => navigation.navigate('PokemonDetail', {data: item})}>
+										<Image source={{uri: item.img, width: 30, height: 30}}/>
+										<Text style={commonStyles.listItemText}>{item.name}</Text>
+									</TouchableOpacity>
+								)}
+			/>
+		</View>
+	);
 };
 
 export default PokemonListComponent;
